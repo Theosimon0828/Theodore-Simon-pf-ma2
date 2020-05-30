0b27b912f03ff2d4f85c0e0a8e79bddc86755d2f
Theosimon0828 <63924343+Theosimon0828@users.noreply.github.com>
2020-05-30 20:42:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//number 1//
+petArray = ["cat","cow","dog"];
+//number 2//
+console.log(petArray[1]);
+//number 3//
+petArray.push("sheep")
+//number 4//
+console.log(petArray.length);
+//numer 5//
+var catObject = 
+    {
+       name: "Blacky",
+       color: "black",
+       age: 2
+
+    };
+//number 6//
+console.log(catObject.age);
+//number 7//
+var catArray =[
+    {
+        name: "Blacky",
+        color: "black",
+        age: 2 
+    }
+];
+//number 8//
+for( var i = 0; i < catArray.length; i++){
+    console.log(catArray[i]);
+}
+//number 9//
+function logToConsole(animal)
+{
+    console.log(animal);
+}
+//nuber 10//
+logToConsole("donkey")
+
